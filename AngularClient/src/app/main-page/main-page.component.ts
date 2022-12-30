464a04198e5de408e818b16f01ce65a6e8e6db2a
serbianman87 <serbianman87@yahoo.com>
2022-12-30 15:04:51 +0100

    init

---
+++ AngularClient/src/app/main-page/main-page.component.ts
@@ -0,0 +1,126 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material/material.module';
+import { DataSource } from '@angular/cdk/collections';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { Country } from '../models/country.model';
+
+const CountrDate : Country [] = [];
+
+@Component({
+  selector: 'app-main-page',
+  standalone: true,
+  imports: [CommonModule, MaterialModule],
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+
+export class MainPageComponent {
+
+  displayColumns: string[] - ['countryName', 'confederationName','Actions'];
+  dataSource = CountryData;
+  isEdit: boolean = false;
+
+  constructor(private service: TeamService,private route: ActivatedRoute, private _router: Router, public dialog: MatDialog) { }
+
+  ngOnInit(): void 
+  {
+    this.GetAllTeams()
+  }
+
+  clickedRow(row:any){
+    this._router.navigate(['/ind-country', row.teamId!])
+
+}
+}
+addCountry(){
+  let dialogRef = this.dialog.open(CreateCountryComponent, {
+    data: {
+      row:{teamId:0, countryName:'', confederationId:0, confederation:{confederationId:0, confederationName:''}},
+      isEdit:false
+    },
+  });
+
+  dialogRef.afterClosed().subscribe((res:any) => {
+    if(!null){
+      this.service.addCountry(res.data).subscribe({
+        next: (x:any)=> {
+          this.GetAllTeams()
+          console.log('The dialog was closed');
+        },
+        error: (e:any) => {
+          console.warn(e)
+        }
+      })
+    }
+    else{
+      console.log('The dialog was closed');
+    }
+
+  });
+}
+
+editCountry(row:Country){
+  let dialogRef = this.dialog.open(CreateCountryComponent, {
+    data: {
+      row:row,
+      isEdit:true
+    },
+  });
+
+  dialogRef.afterClosed().subscribe((res:any) => {
+    if(res != null){
+      this.service.editCountry(res.data).subscribe({
+        next: (x:any)=> {
+          this.GetAllTeams()
+          console.log('The dialog was closed');
+        },
+        error: (e:any) => {
+          console.warn(e)
+        }
+      })
+    }
+
+  });
+}
+
+deleteCountry(row:Country){
+  var teamId = row.teamId
+  let dialogRef = this.dialog.open(DeleteCountryComponent, {});
+
+  dialogRef.afterClosed().subscribe((res:any) => {
+    if(res == true ){
+      this.service.deleteCountry(teamId).subscribe({
+        next: (x:any)=> {
+          this.GetAllTeams()
+          console.log('The dialog was closed');
+        },
+        error: (e:any) => {
+          console.warn(e)
+        }
+      })
+    }
+    else{
+      console.log('The dialog was closed');
+    }
+
+  });
+}
+
+GetAllTeams(){
+  this.service.getAllTeams()
+  .subscribe({
+    next: (x:Country[]) => {
+    this.dataSource = x
+  },
+    error: (e:any) => {
+      console.warn(e)
+    }
+  })
+}
+
+
+
+
+

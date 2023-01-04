af945b4679b37b59d4475af27829288dbbad1889
serbianman87 <serbianman87@yahoo.com>
2023-01-04 15:57:05 +0100

    init

--- AngularClient/src/app/app.module.ts
+++ AngularClient/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MaterialModule } from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +19,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: TableComponent, pathMatch: 'full' }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularClient/src/app/table/table.component.ts
+++ AngularClient/src/app/table/table.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { kovanice } from '../Models/kovanice.model';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatTableDataSource,MatTable } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 
@@ -10,12 +10,11 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
-
 })
 export class TableComponent implements OnInit {
   
-  dataSources!: MatTableDataSource<kovanice>;
-  displayColumns: string[] = ['weight', 'name', 'countryOfOrigin', 'manufactuer', 'price', 'Actions']
+  dataSources: MatTableDataSource<kovanice>;
+  displayColumns: string[] = ['weight', 'name', 'countryOfOrigin', 'manufactuer', 'price', 'Actions'];
 
   constructor(private apiService: ApiService, public dialog: MatDialog, private _snackBar: MatSnackBar) {}
 
@@ -24,12 +23,12 @@ export class TableComponent implements OnInit {
     }
 
 
-    async getAllKovanice() {
-      await this.apiService.getAllKovanice().subscribe((res: kovanice[] | undefined)=>
+  async getAllKovanice() {
+    console.log('this gets the players from db')
+    await this.apiService.getAllKovanice().subscribe((res: kovanice[] | undefined) =>
       {
         this.dataSources = new MatTableDataSource(res);
       });
     }
 
   }
-}
